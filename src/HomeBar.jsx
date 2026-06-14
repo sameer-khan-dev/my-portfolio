2e6ba2a5753c3a4c9423509d343e166f01d516ec
Sameer Khan <252484667+sameer-khan-dev@users.noreply.github.com>
2026-06-14 19:14:27 +0530

    Add download option on small screens

--- src/HomeBar.jsx
+++ src/HomeBar.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react"
+import myResume from "./assets/Sameer_Khan_Frontend_Developer_Resume.pdf"
 
-export function HomeBar(){
+export function HomeBar({changer}){
     let [isVisible,setIsVisible]=useState(true);
     let updateVisibility=()=>{
         setIsVisible(!isVisible);
+        changer();
     }
     if(isVisible){
     return(
-        <div className="absolute  right-0 top-0 flex flex-col text-lg rounded gap-8 text-[#c4b5fd] bg-white w-40 h-70 justify-center items-center ">
+        <div className="absolute  right-0 top-0 flex flex-col text-lg rounded gap-5 text-[#7c3aed] bg-[#0d0d0d]/95 w-40 h-70 justify-center items-center ">
             <span className="h-0" onClick={updateVisibility}><i className="fa-solid fa-xmark relative right-14 bottom-2"></i></span>
-            <a href='#about' className="">About</a>
-            <a href='#skills' className="">Skills</a>
-            <a href='#projects' className="">Projects</a>
-            <a href='#contact' className="">Contact</a>
+            <a href='#about'>About</a>
+            <a href='#skills'>Skills</a>
+            <a href='#projects'>Projects</a>
+            <a href='#contact'>Contact</a>
+            <a href={myResume} download >
+            <i class="fa-solid fa-download"></i> Resume
+            </a>
         </div>
     )
 }else{
